f198953732bc3fd79fe31f95b03ee319d5d1522f
raghunathreddymustur <raghunathreddymustur321@gmail.com>
2021-08-13 22:57:12 +0530

    Data Biding From form to Class- Login and SignUp

--- src/app/Authentication/login/login.component.ts
+++ src/app/Authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  signInForm!:FormGroup
   constructor() { }
 
   ngOnInit(): void {
+    this.signInForm=new FormGroup(
+      {
+        'username':new FormControl(),
+        'password':new FormControl()
+      }
+    )
   }
-
+  signInData()
+  {
+    console.log(this.signInForm.value)
+  }
 }
--- src/app/Authentication/signup/signup.component.ts
+++ src/app/Authentication/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signUpForm!:FormGroup 
 
   constructor() { }
 
   ngOnInit(): void {
+    this.signUpForm=new FormGroup(
+      {
+        'fullName':new FormControl("Enter Your Name"),
+        'email':new FormControl(),
+        'password':new FormControl(),
+        'graduationYear':new FormControl(),
+        'nameOfCollege':new FormControl()
+      }
+    )
   }
-
+  signUpFormData()
+  {
+     console.log(this.signUpForm.value);
+  }
 }
